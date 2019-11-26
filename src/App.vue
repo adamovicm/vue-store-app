c461dfbda54a545440da0122ddfbcdf2a9ced05f
adamovicm <adam.thor@live.com>
2019-11-26 12:14:46 +0100

    task 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ul class="nav">
+      <router-link to="/customers" tag="li" class="nav-link">
+        <a class="nav-link">Customers</a>
+      </router-link>
+      <router-link to="/products" tag="li" class="nav-link">
+        <a class="nav-link">Products</a>
+      </router-link>
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import AppCustomers from './components/AppCustomers.vue';
+import AppProducts from './components/AppProducts.vue';
+
+export const routes = [
+    {path: '/customers', component: AppCustomers},
+    {path: '/products', component: AppProducts}
+]
