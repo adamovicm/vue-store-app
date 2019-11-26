7b9693b7acd1f9857133b246a7c6ba3da78fcad1
adamovicm <adam.thor@live.com>
2019-11-26 13:32:28 +0100

    task 2 done

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,10 +1,50 @@
 <template>
-  <h1>Customers</h1>
+  <div>
+    <h1>Customers</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(customer, index) in customers" :key="index">
+          <td>{{ customer.id }}</td>
+          <td>{{ customer.firstName }}</td>
+          <td>{{ customer.lastName }}</td>
+          <td>{{ customer.email }}</td>
+          <td>
+            <button 
+              class="btn btn-danger"
+              @click.prevent="removeCustomer(index)">Remove</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
+import { customers } from '../services/CustomerService.js';
 export default {
-
+  data() {
+    return {
+      customers
+    }
+  },
+  methods: {
+    removeCustomer(index) {
+      if(confirm('Really delete customer ' 
+        + customers[index].firstName + ' ' 
+        + customers[index].lastName + '?')) {
+        customers.splice(index, 1);
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/services/CustomerService.js
@@ -0,0 +1,20 @@
+export const customers = [
+    {
+        id: 0,
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'john.s@email.com'
+    },
+    {
+        id: 1,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'jane.d@email.com'
+    },
+    {
+        id: 2,
+        firstName: 'Pera',
+        lastName: 'Peric',
+        email: 'pp@email.com'
+    }
+]
